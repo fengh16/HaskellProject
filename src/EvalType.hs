e01842cd1443db569aff67b38f5548a7f05ac6d7
fengh16 <fengh16@163.com>
2019-06-15 09:39:59 +0800

    Update new way

--- src/EvalType.hs
+++ src/EvalType.hs
@@ -3,10 +3,12 @@ module EvalType where
 
 import AST
 import Control.Monad.State
+import Data.Map
 
-data Context = Context { -- 可以用某种方式定义上下文，用于记录变量绑定状态
-                       }
-  deriving (Show, Eq)
+data Context = Context {
+  -- 可以用某种方式定义上下文，用于记录变量绑定状态
+  runContext :: [(String, Expr)]
+} deriving (Show, Eq)
 
 type ContextState a = StateT Context Maybe a
 
@@ -64,9 +66,7 @@ isIf3 eif e1 e2 = do
 -- evalTypeInternal :: Expr -> ContextState Type
 -- evalTypeInternal e1 e2 e3 = do
 --   et1 <- eval e1
---   case et1 of
---     (TData s1) ->
---       Context
+--   getContext
 
 eval :: Expr -> ContextState Type
 eval (EBoolLit _) = return TBool
@@ -94,4 +94,4 @@ eval _ = undefined
 
 evalType :: Program -> Maybe Type
 evalType (Program adts body) = evalStateT (eval body) $
-  Context {  } -- 可以用某种方式定义上下文，用于记录变量绑定状态
+  Context [] -- 可以用某种方式定义上下文，用于记录变量绑定状态
--- src/EvalValue.hs
+++ src/EvalValue.hs
@@ -11,8 +11,10 @@ data Value
   -- ... more
   deriving (Show, Eq)
 
-data Context = Context { -- 可以用某种方式定义上下文，用于记录变量绑定状态
-                          } deriving (Show, Eq)
+data Context = Context {
+  -- 可以用某种方式定义上下文，用于记录变量绑定状态
+  runContext :: [(String, Expr)]
+} deriving (Show, Eq)
 
 type ContextState a = StateT Context Maybe a
 
@@ -23,155 +25,84 @@ getBool e = do
     VBool b -> return b
     _ -> lift Nothing
 
-getAnd :: Expr -> Expr -> ContextState Bool
-getAnd e1 e2 = do
+getTwoBIC e1 e2 = do
   ev1 <- eval e1
   ev2 <- eval e2
   case (ev1, ev2) of
-    (VBool False, _) -> return False
-    (VBool True, VBool b2) -> return b2
+    (VBool b1, VBool b2) -> return (compare b1 b2)
+    (VInt b1, VInt b2) -> return (compare b1 b2)
+    (VChar b1, VChar b2) -> return (compare b1 b2)
     _ -> lift Nothing
 
-getOr :: Expr -> Expr -> ContextState Bool
-getOr e1 e2 = do
+getTwoIC e1 e2 = do
   ev1 <- eval e1
   ev2 <- eval e2
   case (ev1, ev2) of
-    (VBool True, _) -> return True
-    (VBool False, VBool b2) -> return b2
+    (VInt b1, VInt b2) -> return (compare b1 b2)
+    (VChar b1, VChar b2) -> return (compare b1 b2)
     _ -> lift Nothing
 
-getAdd :: Expr -> Expr -> ContextState Int
-getAdd e1 e2 = do
+checkTwoEqual e1 e2 = do
   ev1 <- eval e1
   ev2 <- eval e2
   case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 + b2)
+    (VBool _, VBool _) -> return (ev1, ev2)
+    (VInt _, VInt _) -> return (ev1, ev2)
+    (VChar _, VChar _) -> return (ev1, ev2)
     _ -> lift Nothing
 
-getSub :: Expr -> Expr -> ContextState Int
-getSub e1 e2 = do
+getTwoInt e1 e2 = do
   ev1 <- eval e1
   ev2 <- eval e2
   case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 - b2)
-    _ -> lift Nothing
-
-getMul :: Expr -> Expr -> ContextState Int
-getMul e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 * b2)
-    _ -> lift Nothing
-
-getDiv :: Expr -> Expr -> ContextState Int
-getDiv e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt 0) -> lift Nothing
-    (VInt b1, VInt b2) -> return (b1 `div` b2)
-    _ -> lift Nothing
-
-getMod :: Expr -> Expr -> ContextState Int
-getMod e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt 0) -> lift Nothing
-    (VInt b1, VInt b2) -> return (b1 `mod` b2)
-    _ -> lift Nothing
-
-getEq :: Expr -> Expr -> ContextState Bool
-getEq e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VBool b1, VBool b2) -> return (b1 == b2)
-    (VInt b1, VInt b2) -> return (b1 == b2)
-    (VChar b1, VChar b2) -> return (b1 == b2)
-    _ -> lift Nothing
-
-getNeq :: Expr -> Expr -> ContextState Bool
-getNeq e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VBool b1, VBool b2) -> return (b1 /= b2)
-    (VInt b1, VInt b2) -> return (b1 /= b2)
-    (VChar b1, VChar b2) -> return (b1 /= b2)
-    _ -> lift Nothing
-
-getLe :: Expr -> Expr -> ContextState Bool
-getLe e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 <= b2)
-    (VChar b1, VChar b2) -> return (b1 <= b2)
-    _ -> lift Nothing
-
-getLt :: Expr -> Expr -> ContextState Bool
-getLt e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 < b2)
-    (VChar b1, VChar b2) -> return (b1 < b2)
-    _ -> lift Nothing
-
-getGe :: Expr -> Expr -> ContextState Bool
-getGe e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 >= b2)
-    (VChar b1, VChar b2) -> return (b1 >= b2)
-    _ -> lift Nothing
-
-getGt :: Expr -> Expr -> ContextState Bool
-getGt e1 e2 = do
-  ev1 <- eval e1
-  ev2 <- eval e2
-  case (ev1, ev2) of
-    (VInt b1, VInt b2) -> return (b1 > b2)
-    (VChar b1, VChar b2) -> return (b1 > b2)
-    _ -> lift Nothing
-
-getIfAns :: Expr -> Expr -> Expr -> ContextState Value
-getIfAns eif e1 e2 = do
-  evif <- eval eif
-  case (evif) of
-    (VBool True) -> eval e1
-    (VBool False) -> eval e2
+    (VInt b1, VInt b2) -> return (b1, b2)
     _ -> lift Nothing
 
 eval :: Expr -> ContextState Value
 eval (EBoolLit b) = return $ VBool b
 eval (EIntLit b) = return $ VInt b
 eval (ECharLit b) = return $ VChar b
+
 eval (ENot e) = getBool e >>= \b -> return (VBool $ not b)
-eval (EAnd e1 e2) = getAnd e1 e2 >>= \b -> return (VBool b)
-eval (EOr e1 e2) = getOr e1 e2 >>= \b -> return (VBool b)
-eval (EAdd e1 e2) = getAdd e1 e2 >>= \b -> return (VInt b)
-eval (ESub e1 e2) = getSub e1 e2 >>= \b -> return (VInt b)
-eval (EMul e1 e2) = getMul e1 e2 >>= \b -> return (VInt b)
-eval (EDiv e1 e2) = getDiv e1 e2 >>= \b -> return (VInt b)
-eval (EMod e1 e2) = getMod e1 e2 >>= \b -> return (VInt b)
-eval (EEq e1 e2) = getEq e1 e2 >>= \b -> return (VBool b)
-eval (ENeq e1 e2) = getNeq e1 e2 >>= \b -> return (VBool b)
-eval (ELe e1 e2) = getLe e1 e2 >>= \b -> return (VBool b)
-eval (ELt e1 e2) = getLt e1 e2 >>= \b -> return (VBool b)
-eval (EGe e1 e2) = getGe e1 e2 >>= \b -> return (VBool b)
-eval (EGt e1 e2) = getGt e1 e2 >>= \b -> return (VBool b)
-eval (EIf eif e1 e2) = getIfAns eif e1 e2
+eval (EAnd e1 e2) = do
+  b1 <- getBool e1
+  b2 <- getBool e2
+  return (VBool (b1 && b2))
+eval (EOr e1 e2) = do
+  b1 <- getBool e1
+  b2 <- getBool e2
+  return (VBool (b1 || b2))
+
+eval (EAdd e1 e2) = getTwoInt e1 e2 >>= \(ev1, ev2) -> return (VInt (ev1 + ev2))
+eval (ESub e1 e2) = getTwoInt e1 e2 >>= \(ev1, ev2) -> return (VInt (ev1 - ev2))
+eval (EMul e1 e2) = getTwoInt e1 e2 >>= \(ev1, ev2) -> return (VInt (ev1 * ev2))
+eval (EDiv e1 e2) = getTwoInt e1 e2 >>= \(ev1, ev2) -> (
+    if ev2 /= 0 then return (VInt (div ev1 ev2)) else lift Nothing
+  )
+eval (EMod e1 e2) = getTwoInt e1 e2 >>= \(ev1, ev2) -> (
+    if ev2 /= 0 then return (VInt (mod ev1 ev2)) else lift Nothing
+  )
+
+eval (EEq e1 e2) = getTwoBIC e1 e2 >>= \cmp -> return (VBool (cmp == EQ))
+eval (ENeq e1 e2) = getTwoBIC e1 e2 >>= \cmp -> return (VBool (cmp /= EQ))
+eval (ELe e1 e2) = getTwoIC e1 e2 >>= \cmp -> return (VBool (cmp == EQ || cmp == LT))
+eval (ELt e1 e2) = getTwoIC e1 e2 >>= \cmp -> return (VBool (cmp == LT))
+eval (EGe e1 e2) = getTwoIC e1 e2 >>= \cmp -> return (VBool (cmp == EQ || cmp == GT))
+eval (EGt e1 e2) = getTwoIC e1 e2 >>= \cmp -> return (VBool (cmp == GT))
+
+eval (EIf eif e1 e2) = do
+  evif <- eval eif
+  (ev1, ev2) <- checkTwoEqual e1 e2
+  case evif of
+    (VBool True) -> return ev1
+    (VBool False) -> return ev2
+    _ -> lift Nothing
 -- ... more
 eval _ = undefined
 
 evalProgram :: Program -> Maybe Value
 evalProgram (Program adts body) = evalStateT (eval body) $
-  Context {  } -- 可以用某种方式定义上下文，用于记录变量绑定状态
+  Context [] -- 可以用某种方式定义上下文，用于记录变量绑定状态
 
 
 evalValue :: Program -> Result
